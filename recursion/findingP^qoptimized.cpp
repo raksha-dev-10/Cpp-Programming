134f6e4c1c4255426900e6a7b44a30f3735c6040
raksha-dev-10 <dev.rakshasharma@gmail.com>
2025-01-31 12:51:16 +0530

    optimized

---
+++ recursion/findingP^qoptimized.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int power(int p, int q){
+    if(q==0){
+        return 1;       //base case
+    }
+    if(q%2==0){
+        //q is even
+        int ans = power(p,q/2);
+        return ans*ans;         //assumption and self work
+    }
+    else{
+        //q is odd
+        int ans = power(p,q/2);
+        return p*ans*ans;       //assumption and self work
+    }
+}
+
+int main(){
+    cout<<"enter digit and power: ";
+    int p,q;
+    cin>>p>>q;
+
+    int ans = power(p,q);
+    cout<<ans;
+
+    return 0;
+}
