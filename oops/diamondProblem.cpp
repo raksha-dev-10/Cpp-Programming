879d9d36a438ff2ab61d653ca94fe5c46fc7f7f0
raksha-dev-10 <dev.rakshasharma@gmail.com>
2025-05-12 13:28:32 +0530

    diamond problem

---
+++ oops/diamondProblem.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class parent{
+public:
+    parent(){
+        cout<<"parent"<<endl;
+    }
+};
+
+class child1: public parent{
+public:
+    child1(){
+        cout<<"child1"<<endl;
+    }
+};
+
+class child2: public parent{
+public:
+    child2(){
+        cout<<"child2"<<endl;
+    }
+};
+
+class grandchild: public child1, public child2{
+public:
+    grandchild(){
+        cout<<"grandchild"<<endl;
+    }
+};
+
+int main(){
+    grandchild();
+    
+    return 0;
+}
